4885d20a012b8c6b3634626636abcfdb40ea3865
Muhammad Amray <muhammadamray@gmail.com>
2024-01-26 23:49:14 -0500

    more comments

--- frontend/src/components/Reservation/ReservationModal.js
+++ frontend/src/components/Reservation/ReservationModal.js
@@ -1,18 +1,49 @@
+// import React, { useState } from "react";
+// import { Modal } from "../../context/Modal";
+// import ReservationEdit from "./ReservationEdit";
+// import "./index.css"
+
+// function ReservationModal({ reservation }) {
+//   // const reservation = reservation;
+
+//   const [showModal, setShowModal] = useState(false);
+
+//   return (
+//     <>
+//       <button className="edit-btn" onClick={() => setShowModal(true)}>Edit</button>
+//       {showModal && (
+//         <Modal onClose={() => setShowModal(false)}>
+//           <ReservationEdit
+//             setShowModal={setShowModal}
+//             reservation={reservation}
+//           />
+//         </Modal>
+//       )}
+//     </>
+//   );
+// }
+
+// export default ReservationModal;
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import ReservationEdit from "./ReservationEdit";
-import "./index.css"
+import "./index.css";
 
 function ReservationModal({ reservation }) {
-  // const reservation = reservation;
-
+  // State to manage the visibility of the modal
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button className="edit-btn" onClick={() => setShowModal(true)}>Edit</button>
+      {/* Button to open the modal */}
+      <button className="edit-btn" onClick={() => setShowModal(true)}>
+        Edit
+      </button>
+
+      {/* Modal component that opens when showModal is true */}
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
+          {/* ReservationEdit component rendered within the modal */}
           <ReservationEdit
             setShowModal={setShowModal}
             reservation={reservation}
